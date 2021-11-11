dc663ac9ae7740cd722c118bbd5af759da86d388
PHastings37 <patrickhastings@hotmail.co.uk>
2021-11-11 13:35:08 +0000

    github fucked it but all good. resampler2 works

--- resampler2.py
+++ resampler2.py
@@ -1,3 +1,11 @@
+"""
+an updated resampler that can handle masks, takes in a mask or dicom structure and returns a resampled image of 512x512x256 with spacing of 113 OR however 
+you want to define it. 
+
+adapted from Rorys code MASK_resample.py https://github.com/rfarwell/MPhys
+
+"""
+
 import rt_utils
 import sys
 import numpy as np
@@ -26,15 +34,13 @@ def resample_volume(volume, interpolator, def_pix_val):
     resample.SetSize(new_size)
     resample.SetOutputSpacing(Output_Spacing)
     resample.SetDefaultPixelValue(def_pix_val)
-
     return resample.Execute(volume)
 
 def permute_axes(volume) :
-    """
-    This function permutes the axes of the input volume.
-    """
+   #This function permutes the axes of the input volume.
+    
     permute = sitk.PermuteAxesImageFilter()
-    permute.SetOrder([2,1,0])
+    permute.SetOrder([1,2,0])
 
     return permute.Execute(volume)
 
@@ -54,6 +60,7 @@ for filename in os.listdir(filepath):
         reader.SetFileNames(dcm_paths)
         image = sitk.ReadImage(dcm_paths)
         dicom = resample_volume(image, sitk.sitkLinear, -1024)
+        #dicom = dicom.astype(float)
         sitk.WriteImage(dicom, f"{os.path.join(outputpath, filename)}.nii")
 
     elif "-RT" in filename:
@@ -69,21 +76,21 @@ for filename in os.listdir(filepath):
         mask_3d_spinal_cord = rtstruct.get_roi_mask_by_name("Spinal-Cord")
 
         # Setting what the desired mask is (for the case of a tumour we out GTV-1)
+        #easiest to view if looking at lungs, so good sanity check
         mask_3d = mask_3d_Lung_Right + mask_3d_Lung_Left
 
         #Converting this array from boolean to binary
         mask_3d = mask_3d + 1
         mask_3d = mask_3d - 1
 
+        mask_3d = mask_3d.astype(np.float32)
         mask_3d_image = sitk.GetImageFromArray(mask_3d)
-
+        
+        #sitk.WriteImage(mask_3d_image, f"{outputpath}/testrt.nii")
         mask_3d_image = permute_axes(mask_3d_image)
         mask_3d_image.SetSpacing(dicom.GetSpacing())
-
+        mask_3d_image.SetDirection(dicom.GetDirection())
+        mask_3d_image.SetOrigin(dicom.GetOrigin())
         mask_3d_image_resampled = resample_volume(mask_3d_image, sitk.sitkNearestNeighbor, 0)
-        mask_img = sitk.GetImageFromArray(mask_3d_image_resampled)
 
-        sitk.WriteImage(mask_img, f"{os.path.join(outputpath, filename)}.nii")
-        
-    
-        
+        sitk.WriteImage(mask_3d_image_resampled, f"{os.path.join(outputpath, filename)}.nii")
